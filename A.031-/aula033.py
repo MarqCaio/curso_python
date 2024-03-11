77fec5453e487079e56fcd0300ff9d52a3e61ff3
Caio Marques <caiomarques2665@gmail.com>
2024-03-11 14:56:05 -0300

    Commit

--- A.031-/aula033.py
+++ A.031-/aula033.py
@@ -1,5 +1,9 @@
 '''
+Documentação do python
 https://docs.python.org/pt-br/3/library/stdtypes.html
 imutaveis que vimos: str, int, float, bool
 '''
 string = 'Caio Marques'
+# outra_variavel = f'{string[:4]} da Silva{string[4:]}'
+# print(outra_variavel)
+print(string.zfill(15))
