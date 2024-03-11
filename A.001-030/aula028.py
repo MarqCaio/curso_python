b97f305415d8e07a15b4314455eccac926022e75
Caio Marques <caiomarques2665@gmail.com>
2024-03-11 14:25:23 -0300

    Commit

--- A.001-030/aula028.py
+++ A.001-030/aula028.py
@@ -25,9 +25,8 @@ if nome and idade:
         print('Seu nome contém espaços.')
     else:
         print ('Seu nome NÂO contém espaços.')
-    print(f'O seu nome contém {len(nome)} letras.')
-    print(f'A Primeira letra do seu nome é {nome[0]}.')
-    print(f'A última letra do seu nome é {nome[-1]}.')
-
+        print(f'O seu nome contém {len(nome)} letras.')
+        print(f'A Primeira letra do seu nome é {nome[0]}.')
+        print(f'A última letra do seu nome é {nome[-1]}.')
 else:
     print('Desculpe, você deixou campos vazios.')
