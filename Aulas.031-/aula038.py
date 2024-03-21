9832c940666579485153438cc92dbb0db0a25e38
Caio Marques <caiomarques2665@gmail.com>
2024-03-21 18:47:30 -0300

    aulas while

--- Aulas.031-/aula038.py
+++ Aulas.031-/aula038.py
@@ -1,18 +1,19 @@
 """
-Repetições 
-While (Enquanto)
-Executa uma ação enquanto uma condição for verdadeira
-Loop infinito -> Quando um código não tem fim
+Repetições
+while (enquanto)
+Executar uma ação enquanto uma condição for verdadeira
+loop infinito -> Quando um código não tem fim
 """
-qtd_linhas = 5
-qtd_colunas = 5
+qtd_linha = 5
+qtd_coluna = 5
 
 linha = 1
-while linha <= qtd_linhas:
+while linha <= qtd_linha:
     coluna = 1
-    while coluna <= qtd_colunas:
+    while coluna <= qtd_coluna:
         print(f'{linha=} {coluna=}')
         coluna += 1
-    linha += 1
+    linha =+ 1
 
-print('Acabou')
+
+print('Acabou!')
---
+++ Aulas.031-/aula039.py
@@ -0,0 +1,14 @@
+"""
+Interando strings com while
+"""
+nome = 'Caio Marques' #strings são iteráveis
+
+indice = 0
+novo_nome = ''
+while indice < len(nome):
+    letra = nome[indice]
+    novo_nome += f'*{letra}'
+    indice += 1
+
+novo_nome += '*'
+print(novo_nome)
